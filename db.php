d0665cd2ea2b1a6be5775e709b196660d41c3f9d
Nama Awak <emel.awak@gmail.com>
2026-02-27 03:14:39 +0800

    Tambah sistem kesan ralat untuk selesaikan isu 500

--- db.php
+++ db.php
@@ -1,4 +1,8 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+mysqli_report(MYSQLI_REPORT_OFF);
+
 $host = getenv('MYSQLHOST') ?: "localhost";
 $user = getenv('MYSQLUSER') ?: "root";
 $pass = getenv('MYSQLPASSWORD') ?: "";
